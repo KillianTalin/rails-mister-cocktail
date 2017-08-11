79ba25a1b0f76b2623e5b5ccfcdefa05eaed1dd5
Killian Talin <killian.talin@live.fr>
2017-08-11 15:55:18 +0200

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
 
-  mount Attachinary::Engine => "/attachinary"
-
   resources :cocktails, only: [ :index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [ :destroy ]
+
+  mount Attachinary::Engine => "/attachinary"
 end
